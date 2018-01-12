a08e832fbcc7e47d2f7f4c08dc8506b81a5f174a
H7819674 <h7819674@mac-0070638.local>
2018-01-12 14:45:35 +0000

    Convert to MVC Design Pattern

---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  ToDoey
+//
+//  Created by H7819674 on 12/01/2018.
+//  Copyright © 2018 Virgin Media. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+    
+}
